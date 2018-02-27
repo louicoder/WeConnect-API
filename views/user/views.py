8b229a0f27f03a7b1a279e8b1d51de566aa91c75
louiCoder <louim2010@gmail.com>
2018-02-28 01:52:51 +0300

    added routes getusers, resetpassowrd, logout and ignored pycache files

--- views/user/views.py
+++ views/user/views.py
@@ -21,8 +21,65 @@ def createuser():
 @userBlueprint.route('/api/v1/auth/getusers', methods=['GET'])
 def getusers():
     global USERS
-    usersx = USERS
-    return jsonify({"USERS":usersx})
+    if not USERS:
+        return jsonify({'message':'No users found in the system'})
+    else:
+        usersx = USERS
+        return jsonify({"USERS":usersx})
 
+@userBlueprint.route('/api/v1/auth/login', methods=['POST'])
+def login():
+    global loggedInUser
+    username=request.json['username']
+    password=request.json['password']
+
+    if USERS:
+        for x in USERS:
+            for k in x:
+                if x['username'] == username and x['password'] == password:
+                    loggedInUser.append({'username':username, 'password':password})
+                    return make_response(jsonify({'message':'login successful'})), 200
+                else:
+                    return make_response(jsonify({'message':'login unsuccessful'})), 401
+    else:
+        return make_response(jsonify({'message':'No users found in the system.'})), 404
+
+@userBlueprint.route('/api/v1/auth/resetpassword', methods=['PUT'])
+def resetPassword():
+    global USERS
+    global loggedInUser
+    username=''
+    for x in loggedInUser:
+        for y in x:
+            username = x['username']
+
+    if not USERS:
+        return jsonify({'message': 'no users found in system'})
+    else:
+        for x in USERS:
+            for k in x:
+                if x['username'] == username:
+                    x['password'] = 'password'
+                    print(USERS)
+                    return make_response(jsonify({'message':'password Reset successful'})), 200
+                else:
+                    return make_response(jsonify({'message':'password change was not unsuccessful'})), 401
+
+@userBlueprint.route('/api/v1/auth/logout', methods=['POST'])
+def logout():
+    global loggedInUser
+    if loggedInUser:
+        loggedInUser = None
+        return jsonify({'message':'You are now logged out'})
+    else:
+        return make_response(jsonify({'message':'No one logged in so far'}))
+
+@userBlueprint.route('/api/v1/auth/getlogged', methods=['GET'])
+def logged():
+    global loggedInUser
+    if loggedInUser:       
+        return jsonify({'loggedInUser':loggedInUser})
+    else:
+        return make_response(jsonify({'message':'No one logged in so far'}))
 
 
